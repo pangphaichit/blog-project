bbf9eb686f6c9689c488e2b5f071e83551a84580
pangphaichit <pg_grj@hotmail.com>
2024-11-07 15:20:05 +0700

    fix: serverless deployment and update routes

--- app.mjs
+++ app.mjs
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 4001;  
 
 app.use(cors());
 app.use(express.json());
@@ -15,18 +14,9 @@ const userProfile = {
   }
 };
 
-
 app.get("/profiles", (req, res) => {
-  return res.status(200).json(userProfile);  
+  return res.status(200).json(userProfile); 
 });
-
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-}
-
 
 export default app;
 
