28f43b3563b95ee5694548f425879b366b992d1b
pangphaichit <pg_grj@hotmail.com>
2024-11-07 14:59:51 +0700

    refactor: route handling in app.mjs

--- app.mjs
+++ app.mjs
@@ -1,24 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/profiles", (req, res) => {
-  const userProfile = {
-    data: {
-      name: "john",
-      age: 20
-    }
-  };
-
-  if (!userProfile) {
-    return res.status(404).json({
-      error: 'User profile not found'
+    return res.json({
+      data: {
+        name: "john",
+        age: 20,
+      },
     });
-  }
-
-  res.status(200).json(userProfile);
-});
+  });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
